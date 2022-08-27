d307c93e9bda8c8d907eabe7d1b6b047cc24e424
JamTheDev <2004jamvillarosa@gmail.com>
2022-08-27 11:55:59 +0800

    Revert "fix: removed .env because im stupid n i forgot"
    This reverts commit 6b24cd33f76582be6d18fe4bce768309a88fd9f7.

--- helpers/octokit.ts
+++
@@ -1,4 +0,0 @@
-import { Octokit } from '@octokit/core';
-const octokit = new Octokit({ auth: process.env.REPO_KEY! });
-
-export default octokit;
--- pages/api/commits.ts
+++ pages/api/commits.ts
@@ -1,16 +1,18 @@
 import { NextApiHandler } from "next";
-import octokit from "helpers/octokit";
+import { Octokit } from '@octokit/core';
+
+const octokit = new Octokit({ auth: process.env.REPO_KEY });
 
 const handler: NextApiHandler = async (req, res) => {
     const fiveRecentCommits = await octokit.request(
-        'GET /repos/JamTheDev/personal-web/commits', {per_page: 5},
+        'GET /repos/JamTheDev/personal-web/commits', {per_page: 5}
     )
 
     if (fiveRecentCommits.status == 200) {
         return res.status(200).json(fiveRecentCommits);
     }
 
-    return res.status(400).json(fiveRecentCommits.data)
+    return res.status(400).json({ message: "bad request" })
 }
 
 export default handler;
